d15c3221bb2d7e2043412cfea9c79efec4e20c74
Greg Cerveny <greg.cerveny@gmail.com>
2014-01-09 08:46:55 -0600

    removing source

--- heyyou.js
+++
@@ -1,4 +0,0 @@
-// phone number: (123) 456-7890
-var match = /\(\d{3}\) \d{3}\-\d{4}/g;
-var link = "<a href='#' onClick='alert(" + '"Hey You"' + ");return false;'>$&</a>";
-document.body.innerHTML = document.body.innerHTML.replace(match, link);
